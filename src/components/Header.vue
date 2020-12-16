9f8b666608f8913ffccfaaba7c483b3bace0fcd6
Julian Hahn <julian.hahn@student.fh-kiel.de>
2020-12-16 23:10:56 +0100

    use shorthand in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,17 @@
 <template>
    <header style="-webkit-app-region: drag" class="flex h-12 w-full px-4 shadow-lg bg-gray-900">
         <div class="text-2xl my-auto">UKSH VR Auswertung</div>
-        <button v-on:click="minimize" class="ml-auto justify-end mr-4" style="-webkit-app-region: no-drag">
+        <button @click="minimize" class="ml-auto justify-end mr-4" style="-webkit-app-region: no-drag">
             <svg class="w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 12H6" />
             </svg>
         </button>
-        <button v-on:click="maximize" class="mr-4 justify-end" style="-webkit-app-region: no-drag">
+        <button @click="maximize" class="mr-4 justify-end" style="-webkit-app-region: no-drag">
             <svg class="w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" >
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4" />
             </svg>
         </button>
-        <button v-on:click="close" class="mr-4 justify-end" style="-webkit-app-region: no-drag">
+        <button @click="close" class="mr-4 justify-end" style="-webkit-app-region: no-drag">
             <svg class="w-7" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
@@ -29,6 +29,7 @@ setup(){
     function close(){
         window.close();
     }
+
     function minimize(){
         window.minimize();
     }
